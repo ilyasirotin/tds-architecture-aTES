2a68bf1390c83bcae0309428002ea0d1c7bbfa6a
Ilya Sirotin <ilyasirotin.we@gmail.com>
2024-03-01 17:46:58 +0500

    [week-2] Added Oauth2 server library

--- week_2/auth/config/bundles.php
+++ week_2/auth/config/bundles.php
@@ -9,4 +9,6 @@ return [
     Symfony\Bundle\DebugBundle\DebugBundle::class => ['dev' => true],
     Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
     Twig\Extra\TwigExtraBundle\TwigExtraBundle::class => ['all' => true],
+    Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
+    League\Bundle\OAuth2ServerBundle\LeagueOAuth2ServerBundle::class => ['all' => true],
 ];
