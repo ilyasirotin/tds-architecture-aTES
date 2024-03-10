ff68c16785473ef6f2735332b3c58ea0f181bbdb
Ilya Sirotin <ilyasirotin.we@gmail.com>
2024-03-10 17:08:40 +0500

    [week-2] Remove unnecessary symfony bundles

--- app/billing/config/bundles.php
+++ app/billing/config/bundles.php
@@ -10,4 +10,5 @@ return [
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
     Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
     KnpU\OAuth2ClientBundle\KnpUOAuth2ClientBundle::class => ['all' => true],
+    Enqueue\Bundle\EnqueueBundle::class => ['all' => true],
 ];
